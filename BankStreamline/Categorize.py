f5a7e04d058764d210dee8701e367f165593df1f
blakedennett <blakedennett@outlook.com>
2026-05-10 08:41:24 -0500

    added april 2026

--- BankStreamline/Categorize.py
+++ BankStreamline/Categorize.py
@@ -43,6 +43,7 @@ def make_categories(df):
         .when(pl.col('description').str.contains('T J MAXX|OLD NAVY|SHEIN|REAL DEALS|SALLY BEAUTY|REXBURG DI')).then(pl.lit('shopping'))
         .when(pl.col('description').str.contains('LDS DIST ONLINE STORE|DC ULTA|DC H&amp|SEPHORA.COM|SALONCENTRIC')).then(pl.lit('shopping'))
         .when(pl.col('description').str.contains('American Eagle|SPORTSMANS WAREHOUSE|ZOE FRYE HAIR|OLDNAVY')).then(pl.lit('shopping'))
+        .when(pl.col('description').str.contains('5230 EL DORADO')).then(pl.lit('shopping'))
                     # ======================= amazon =======================
         .when(pl.col('description').str.contains('AMZN|AMAZON|Amazon.com|temu.com|eBay') & 
             ~pl.col('description').str.contains('AMAZON PRIME')).then(pl.lit('amazon'))
@@ -50,6 +51,7 @@ def make_categories(df):
         .when(pl.col('description').str.contains('Amazon Prime|YouTube|ZOO|MUSEUM|AQUARIUM|LION.S CLUB GOLF|PLAYSTATION NETWORK')).then(pl.lit('fun'))
         .when(pl.col('description').str.contains('COMFORT INN|CINEMA|HOLIDAY INN|El Dorado Golf|PlayStation|ROCK GYM|GRAVITY FACTORY')).then(pl.lit('fun'))
         .when(pl.col('description').str.contains('EXCALIBUR FAMILY FUN|AIRBNB|EXPEDIA|EL DORADO GOLF|Prime Video|AMAZON PRIME')).then(pl.lit('fun'))
+        .when(pl.col('description').str.contains('CENTRAL ARKANSAS LIBRA')).then(pl.lit('fun'))
         .when((pl.col('description').str.contains('UNITED')) & ~(pl.col('description').str.contains('AG AND TURF'))).then(pl.lit('fun'))
                     # ======================= power =======================
         .when(pl.col('description').str.contains('ENTERGY')).then(pl.lit('power'))
